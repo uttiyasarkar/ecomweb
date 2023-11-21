904c38dde95c4a1dfc848fdd59aefe09af7714a4
uttiyasarkar <uttiyasarkar@gmail.com>
2023-11-21 20:13:23 +0100

    feat: change in dir

--- assets/script.js
+++ assets/script.js
@@ -26,3 +26,15 @@ document.getElementById('range-picker').addEventListener('click', function(e) {
 	document.getElementById('color-name').innerHTML = "BLUES / 2V5";
 	document.getElementById('color-name').style.color = '#457'
   })
+  // Example JavaScript for adding products to the cart
+const addToCartButtons = document.querySelectorAll('button');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const productName = button.parentElement.querySelector('h2').innerText;
+        const productPrice = button.parentElement.querySelector('p:nth-child(3)').innerText;
+
+        // You can customize this part to suit your needs, like adding items to a shopping cart object
+        alert(`Added to Cart: ${productName} - ${productPrice}`);
+    });
+});
